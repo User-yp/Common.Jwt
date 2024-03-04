ea429e17985736d4f79c04108bfe9d58deb17520
鹏叶 <1277206411yp@gmail.com>
2024-03-04 13:14:15 +0800

    添加项目文件。

---
+++ Common.Jwt/ModuleInitializer.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common.Jwt
+{
+    class ModuleInitializer : IModuleInitializer
+    {
+        public void Initialize(IServiceCollection services)
+        {
+            services.AddScoped<ITokenService, TokenService>();
+        }
+    }
+}
